d787f93f15f7fb95ff7727c6fb028db0990488a8
tastafur <ruben.romero@guidesmiths.com>
2018-09-20 01:11:44 +0200

    update path index.html for replace

---
+++ bin/update-path.js
@@ -0,0 +1,18 @@
+const replace = require('replace-in-file');
+
+const options = {
+  files: 'index.html',
+  from: /dist\/bundle.js/g,
+  to: (matchWay) => {
+    console.log('Updates path match..:', matchWay);
+    return '/bundle.js'
+  },
+};
+
+try {
+  const changes = replace.sync(options);
+  console.log('Modified files:', changes.join(', '));
+}
+catch (error) {
+  console.error('Error occurred:', error);
+}
